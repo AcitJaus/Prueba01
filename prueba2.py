59c88e21caab204b94ab878529bf6533fb632bea
AcitJaus <diegouuu99@gmail.com>
2026-06-11 20:09:08 -0600

    AÑADI MAS TEXTO

--- prueba2.py
+++ prueba2.py
@@ -1,3 +1,21 @@
 print("Hello, World!")
 print("This is a test script.")
 print("It is used to demonstrate how to complete code snippets.")
+
+print("The code snippet is now complete.")
+print("You can run this script to see the output.")
+print("Goodbye, World!")
+
+
+
+
+
+
+
+
+
+
+
+
+print("End of the script.")
+print("This line will not be executed due to the syntax error above.") 
